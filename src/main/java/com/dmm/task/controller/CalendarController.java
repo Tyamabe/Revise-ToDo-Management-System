06c0c08c45afdb5475cb5ce62e9764d88217fe8b
TaiheiYamabe <91378148+Tyamabe@users.noreply.github.com>
2024-04-23 16:50:39 +0900

    Git連携

--- src/main/java/com/dmm/task/controller/CalendarController.java
+++ src/main/java/com/dmm/task/controller/CalendarController.java
@@ -1,15 +1,18 @@
 package com.dmm.task.controller;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.dmm.task.data.entity.Tasks;
 import com.dmm.task.data.repository.RegistRepository;
@@ -21,9 +24,11 @@ public class CalendarController {
     private RegistRepository registRepository; //タスクデータを取得するため。 RegistRepository の注入
     
     @GetMapping("/main")
-    public String showCalendar(Model model) {
+    public String showCalendar(@RequestParam Optional<String> date, Model model) {
+        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd"); //yyyy-mm-ddのパターンを指定
+        LocalDate today = date.map(d -> LocalDate.parse(d, dateFormatter)).orElse(LocalDate.now()); //Optional→nullかもしれない時。値をLocalDateオブジェクトへ
+        
     	List<List<LocalDate>> monthList = new ArrayList<>();
-        LocalDate today = LocalDate.now();
         LocalDate firstDayOfMonth = today.withDayOfMonth(1);
         int daysFromSunday = firstDayOfMonth.getDayOfWeek().getValue() % 7;
         LocalDate calendarStart = firstDayOfMonth.minusDays(daysFromSunday);
@@ -45,11 +50,15 @@ public class CalendarController {
             LocalDate taskDate = task.getDate().toLocalDate(); // LocalDateTimeからLocalDateへ
             tasksMap.computeIfAbsent(taskDate, k -> new ArrayList<>()).add(task);
         }
+        
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy年 MM月");
+        String formattedDate = today.format(formatter);
 
         model.addAttribute("matrix", monthList);
         model.addAttribute("prev", today.minusMonths(1));
         model.addAttribute("next", today.plusMonths(1));
         model.addAttribute("tasks", tasksMap);
+        model.addAttribute("month", formattedDate);
         return "main";
     }
 }
